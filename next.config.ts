510e226e84b6a3a9b6f98fb54fd93e80eef72856
Milangali Kimu <kimumilangali@gmail.com>
2025-06-12 00:27:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'proudzanzibartours.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
